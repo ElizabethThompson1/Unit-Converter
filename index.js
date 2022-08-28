c3ee484ca558df17ceec4e55ed7e3d1e4e689891
ElizabethThompson1 <usawsoccer11@yahoo.com>
2022-08-28 18:15:40 -0500

    styled it using figma and added more functions to convert the opposit functions

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ let input = document.getElementById("input-el");
 
 
 button.addEventListener("click", function () {
-    meter.innerText = `${input.value} meters = ${convertMeter(input.value)} feet | ${input.value}feet = ${convertFeet(input.value)} meters `;
+    meter.innerText = `${input.value} meters = ${convertMeter(input.value)}  feet | ${input.value}feet = ${convertFeet(input.value)} meters `;
 
-    liter.innerText = `${input.value} liters = ${convertLiters(input.value)} gallons | ${input.value}gallons = ${convertGallons(input.value)} liters `;
+    liter.innerText = `${input.value} liters = ${convertLiters(input.value)} gallons | ${input.value} gallons = ${convertGallons(input.value)} liters `;
 
-    mass.innerText = `${input.value} kilos = ${convertKilograms(input.value).toFixed(3)} pounds | ${input.value}pounds = ${convertPounds(input.value).toFixed(3)} kilos `;
+    mass.innerText = `${input.value} kilos = ${convertKilograms(input.value).toFixed(4)} pounds | ${input.value} pounds = ${convertPounds(input.value).toFixed(3)} kilos `;
 
     input.value = ""
 })
 
 
 function convertGallons(gallons) {
-    return gallons * 3.785.toFixed(3);
+    return gallons * 3.785.toFixed(4);
 }
 function convertPounds(pounds) {
-    return pounds * 1.201.toFixed(3);
+    return pounds * .45359237.toFixed(4);
 }
 
 function convertFeet(feet) {
-    return feet * 3.2808.toFixed(3);
+    return feet * .3048.toFixed(4);
 }
 
 function convertKilograms(kilograms) {
-    return kilograms * 2.204.toFixed(3);
+    return kilograms * 2.204.toFixed(4);
 }
 
 function convertMeter(meter) {
-    return meter * 3.281.toFixed(3);
+    return meter * 3.281.toFixed(4);
 }
 function convertLiters(liters) {
-    return liters * 0.264172.toFixed(3);
+    return liters * 0.264172.toFixed(4);
 }
